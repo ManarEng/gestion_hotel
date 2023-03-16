33b41dc14afa908788115b84fe84571b10400037
safae01 <sasabenmouna@gmail.com>
2023-03-16 23:20:45 +0100

    Update orientation.php

--- PHP/orientation.php
+++ PHP/orientation.php
@@ -2,11 +2,11 @@
 session_start();
 
 if (!isset($_SESSION['ID_UTILL'])) {
-  header("Location: /php/form_connexion.php");
+  header("Location: /PHP/form_connexion.php");
   exit();
 }
 else{
-    header("Location: reservation");
+    header("Location: /PHP/Form_Reservation.php");
     exit();   
 }
 ?>
