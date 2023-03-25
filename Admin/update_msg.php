24d35b76fd1af9f5c15132be4163d0085e689563
safae01 <sasabenmouna@gmail.com>
2023-03-25 14:51:01 +0000

    Update update_msg.php

--- Admin/update_msg.php
+++ Admin/update_msg.php
@@ -5,10 +5,10 @@
     }
     if(isset($_POST['ID_MESSAGE'])) {
         $ID_MESSAGE = $_POST['ID_MESSAGE'];
-        $nom = $_POST['NOM'];
-        $prenom = $_POST['PRENOM'];
-        $email = $_POST['EMAIL'];
-        $message = $_POST['MESSAGE'];
+        $nom = $_POST['nom'];
+        $prenom = $_POST['prenom'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
         $sql = "UPDATE messagerie SET NOM='$nom', PRENOM='$prenom', EMAIL='$email',  MESSAGE='$message' WHERE ID_MESSAGE='$ID_MESSAGE'";
         if ($conn->query($sql) === TRUE) {
             header("Location: MsgAdmin.php");
