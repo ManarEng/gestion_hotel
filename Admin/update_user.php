97e236fe77521176c261f546ea91bb09a2cb373c
DouaeCh <douaechadid@gmail.com>
2023-03-25 16:40:13 +0000

    Update update_user.php

--- Admin/update_user.php
+++ Admin/update_user.php
@@ -11,19 +11,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nom = $_POST['nom'];
     $email = $_POST['email'];
     $tele = $_POST['tele'];
-    $nom_util = $_POST['nom_util'];
+
     $mdp = $_POST['mdp'];
     $cin = $_POST['cin'];
     $adresse = $_POST['adresse'];
+    //verification
 
+    //end of verification
     // Update user information in the database
-    $query = "UPDATE utilisateurs SET prenom='$prenom', nom='$nom', email='$email', tele='$tele', nom_util='$nom_util', mdp='$mdp', cin='$cin', adresse='$adresse' WHERE id_util=$user_id";
-    $result = mysqli_query($conn, $query);
+    if (!empty($mdp)) {
 
+
+        $query = "UPDATE utilisateurs SET prenom='$prenom', nom='$nom', email='$email', tele='$tele', mdp='$mdp', cin='$cin', adresse='$adresse' WHERE id_util=$user_id";
+        $result = mysqli_query($conn, $query);
+    } else {
+        $query = "UPDATE utilisateurs SET prenom='$prenom', nom='$nom', email='$email', tele='$tele', cin='$cin', adresse='$adresse' WHERE id_util=$user_id";
+        $result = mysqli_query($conn, $query);
+    }
     // Close database connection
     mysqli_close($conn);
 
     // Redirect to user profile page
-    header("Location: index_admin.php?id=$user_id");
+    header("Location: profil.php?id=$user_id");
     exit;
 }
