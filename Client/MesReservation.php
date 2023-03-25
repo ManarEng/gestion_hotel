0bfa0c54526b42471a1f894f64eab0255786495d
safae01 <sasabenmouna@gmail.com>
2023-03-25 17:51:59 +0000

    Update MesReservation.php

---
+++ Client/MesReservation.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <link href="/CSS/styles.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Crete+Round' rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"> <!--this one for connexion styles-->
+    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="/Client/style_profile.css">
+    <!--this script is for social medio icons-->
+    <script src="https://kit.fontawesome.com/fe1484d902.js" crossorigin="anonymous"></script>
+
+    <title>HoteLUX</title>
+
+    <script src="/JS/scripts.js"></script>
+    <style>
+        .fa-solid{
+            font-size: 15px;
+
+        }
+    </style>
+     
+</head>
+
+<body>
+    <header>
+        <div class="">
+            <h1 style="text-align: left; margin-left: 10px; margin-top:11px ;"> <a href="/Client/index.php">  HoteLUX<span class="orange">.</span></a></h1>
+            <nav style="margin-top:35px;">
+                <ul>
+                    <li><a href="/Client/index.php/#main" >Accueil</a></li>
+                    <li><a href="/Client/index.php/#steps">A propos</a></li>
+                    <li><a href="/Client/index.php/#possibilities">Services</a></li>
+                    <li><a href="/index_contact.php">Contact</a></li>
+                    <li><a href="">Réservation</a></li>
+                    <li>  <a  href="/Client/gestion_client.php"> <i class="fa-solid fa-user"></i></a></li>
+                    
+
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+   
+
+
+<div class="container">
+
+   <div class="profile">
+      
+            <img src="/Img/default-avatar.png">
+            <h3>Nom d'utilisateur</h3> 
+            <table style="border: 1;">
+                <tr><th>Type de chambre </th><th>Nombre de chambre</th><th>Activité</th><th>date d'arrivée</th><th>date de depart</th></tr>
+            </table>
+        
+   </div>
+
+</div>
+<footer>
+         <div class="col-right">
+            <h3>Contact Info</h3>
+            <p>06 10 30 40 56</p>
+            <p>05 10 30 40 56</p>
+            <p>hotelux@gmail.com</p>
+         </div>
+
+         <div>
+            <h1> <a href="/Client/index.php">HoteLUX<span class="orange">.</span></a></h1>
+            <p class="copyright">Copyright © Tous droits réservés.</div>
+            </p> 
+        </div>
+
+         <div class="col-left">
+            <h3>Contact Info</h3>
+            <p>123,XYZ Road, BSK 3 <br>Banglore, Karnataka, IN</p>
+            <div class="social-icons">
+                <i class="fa-brands fa-facebook" onclick="facebook()"></i>
+                <i class="fa-brands fa-twitter" onclick="twitter()"></i>
+                <i class="fa-brands fa-instagram" onclick="instagram()"></i>
+
+
+            </div>
+         </div>     
+     </footer>
+</body>
+
+</html>
+ 
