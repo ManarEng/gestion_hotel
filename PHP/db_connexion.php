c43958c06240e5405b4ab59c28d33478a6487ea1
safae01 <sasabenmouna@gmail.com>
2023-03-27 13:46:55 +0000

    Delete db_connexion.php

--- PHP/db_connexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$conn = mysqli_connect("localhost","root","");
-
-if( !$conn) { echo "Desolé, connexion à localhost impossible"; exit; }
-if( !mysqli_select_db($conn,'hotelux')) { echo "Désolé, accès à la base gestion_hotel impossible"; exit; }
-
-?>
