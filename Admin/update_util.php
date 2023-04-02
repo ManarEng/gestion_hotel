2859a2d8287c5cc7ebe746dd9dbd584569eda5e6
DouaeCh <douaechadid@gmail.com>
2023-04-02 02:32:21 +0000

    Update update_util.php

--- Admin/update_util.php
+++ Admin/update_util.php
@@ -15,15 +15,36 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $mdp = $_POST['mdp'];
     $cin = $_POST['cin'];
     $adresse = $_POST['adresse'];
+    $file = $_FILES['img'];
+    if (isset($_FILES['img']) && !empty($_FILES['img']['name'])) {
+        // file upload and processing code goes here
+        $allowed_extensions = ['jpg', 'jpeg', 'png'];
+        $file_extension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+        if (!in_array($file_extension, $allowed_extensions)) {
+            echo "Télécharger une image valide.";
+        }
+
+        // Store the image in the upload directory
+        $upload_dir = 'clients/';
+        $filename = uniqid("IMG-", true) . '.' . $file_extension;
+        $upload_path = $upload_dir . $filename;
+        move_uploaded_file($file['tmp_name'], $upload_path);
+
+        // Store the URL in the database
+        $url =  $filename;
+    } else {
+        echo "Veuillez choisir une image à télécharger.";
+    }
+
     //verification
 
     //end of verification
     // Update user information in the database
     if (!empty($mdp)) {
-        $query = "UPDATE utilisateurs SET PRENOM='$prenom', NOM='$nom', E_MAIL='$email', TELE='$tele',MDP='$mdp', CIN='$cin', ADRESSE='$adresse' WHERE ID_UTILL=$user_id";
+        $query = "UPDATE utilisateurs SET PRENOM='$prenom', NOM='$nom', E_MAIL='$email', TELE='$tele',MDP='$mdp', CIN='$cin', ADRESSE='$adresse',IMAGE_UTIL='$url' WHERE ID_UTILL=$user_id";
         $result = mysqli_query($conn, $query);
     } else {
-        $query = "UPDATE utilisateurs SET PRENOM='$prenom', NOM='$nom', E_MAIL='$email', TELE='$tele', CIN='$cin', ADRESSE='$adresse' WHERE ID_UTILL=$user_id";
+        $query = "UPDATE utilisateurs SET PRENOM='$prenom', NOM='$nom', E_MAIL='$email', TELE='$tele', CIN='$cin', ADRESSE='$adresse',IMAGE_UTIL='$url' WHERE ID_UTILL=$user_id";
         $result = mysqli_query($conn, $query);
     }
     // Close database connection
